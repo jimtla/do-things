1871b7e35e26838a7e66ead893e9a09b091d804e
Jim Grandpre <jimtla@gmail.com>
2016-01-31 18:08:54 -0500

    working users api

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-extern crate rustc_serialize;
-use std::io::prelude::*;
-use std::error::Error;
-use rustc_serialize::json::{self, Json, ToJson};
-use rustc_serialize::Encodable;
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
 
+use std::error::Error;
+
+#[macro_use]
 extern crate iron;
 use iron::Iron;
 use iron::method::Method;
@@ -14,93 +14,41 @@ use iron::request::Request;
 extern crate router;
 use router::Router;
 
-extern crate rethinkdb;
-use rethinkdb::r::*;
-use rethinkdb::RethinkDB;
+extern crate rethink;
+use rethink::Rethink;
 
 extern crate rand;
 
 
-mod db {
-    extern crate plugin;
-    extern crate typemap;
-    extern crate rethinkdb;
-
-    use std::sync::{Mutex, Arc, MutexGuard};
-    use self::plugin::Extensible;
-
-    use rethinkdb::RethinkDB;
-
-
-
-    /*
-    pub struct RethinkdbMiddleware {
-        pub pool: Arc<Mutex<rethinkdb::RethinkDB>>,
-    }
-
-    impl RethinkdbMiddleware {
-        pub fn new(host: &str, port: u16, auth: &str, connections: usize) ->
-            RethinkdbMiddleware {
-            let db = RethinkDB::connect(host, port, auth, connections);
-
-            RethinkdbMiddleware {
-                pool: Arc::new(Mutex::new(db)),
-            }
-        }
-    }
-
-    struct Key;
-    impl Middleware for RethinkdbMiddleware {
-        fn invoke<'a>(&self, req: &mut Request, res: Response<'a>) ->
-            MiddlewareResult<'a> {
-                req.extensions_mut().insert::<Key>(self.pool.clone());
-                Ok(Continue(res))
-        }
-    }
-
-    impl typemap::Key for Key {
-        type Value = Arc<Mutex<RethinkDB>>;
-    }
-
-    pub trait RethinkdbRequestExtensions<'a> {
-        fn db_conn(&self) -> MutexGuard<RethinkDB>;
-    }
-
-    impl<'a, 'b, 'c> RethinkdbRequestExtensions<'a> for Request<'a, 'b, 'c> {
-        fn db_conn(&self) -> MutexGuard<RethinkDB> {
-            self.extensions().get::<Key>().unwrap().lock().unwrap()
-        }
-
-    }
-    */
-}
-//use db::RethinkdbRequestExtensions;
+extern crate serde_json;
+extern crate serde;
 
 mod error {
-    use std::convert::From; 
+    use std::convert::From;
     use std::fmt::{self, Display, Formatter};
-
-    extern crate rethinkdb;
-    use rethinkdb::RethinkDBError;
-
-    extern crate rustc_serialize;
-    use rustc_serialize::json::EncoderError;
 
     use std::error::Error;
 
     use std::io;
     use std::rc::Rc;
 
+    use rethink;
+
     #[derive(Debug)]
     pub struct E {
         description: String,
     }
 
+    impl E {
+        pub fn new(description: String) -> Self {
+            E { description: description }
+        }
+    }
+
     impl Display for E {
-        fn fmt(&self, f : &mut Formatter) -> Result<(), fmt::Error> {
+        fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
             self.description().fmt(f)
         }
-
     }
 
     impl Error for E {
@@ -109,125 +57,248 @@ mod error {
         }
     }
 
-    impl From<RethinkDBError> for E {
-        fn from(err : RethinkDBError) -> E {
-            E{description: format!("{:?}", err)}
-        }
-    }
-
     impl From<io::Error> for E {
-        fn from(err : io::Error) -> E {
-            E{description: format!("{:?}", err)}
+        fn from(err: io::Error) -> E {
+            E { description: format!("{:?}", err) }
         }
     }
 
     impl From<Rc<io::Error>> for E {
-        fn from(err : Rc<io::Error>) -> E {
-            E{description: format!("{:?}", err)}
+        fn from(err: Rc<io::Error>) -> E {
+            E { description: format!("{:?}", err) }
         }
     }
 
-    impl From<EncoderError> for E {
-        fn from(err : EncoderError) ->  E {
-            E{description: format!("{:?}", err)}
+    impl From<rethink::Error> for E {
+        fn from(err: rethink::Error) -> E {
+            E { description: format!("{:?}", err) }
         }
     }
 }
 
 mod models {
+
+    pub mod persisted {
+        use rethink::{Datum, Connection, Rethink, Response_ResponseType};
+        use super::super::error::E;
+
+        #[derive(Debug, PartialEq, Eq, Clone)]
+        pub struct Id(String);
+        impl Id {
+            fn str(&self) -> &str {
+                self.0.as_str()
+            }
+
+            pub fn from_string<S: Into<String>>(id: S) -> Self {
+                Id(id.into())
+            }
+        }
+
+        pub trait Persistable : Sized {
+            fn table() -> &'static str;
+            fn to_db(&self) -> Datum;
+            fn from_db(&Datum) -> Result<Self, E>;
+
+            fn insert(self, c: &mut Connection) -> Result<Persisted<Self>, E> {
+                let d = self.to_db();
+                let res = try!(Rethink::table(Self::table()).insert(&d, None).run(c));
+                if res.response_type == Response_ResponseType::RUNTIME_ERROR {
+                    return Err(E::new(format!("{:?}", res.result)));
+                }
+
+                let id = if let Some(&Datum::Object(ref r)) = res.result.get(0) {
+                    if let Some(&Datum::Array(ref keys)) = r.get("generated_keys") {
+                        if let Some(&Datum::String(ref key)) = keys.get(0) {
+                            Some(key)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                };
+
+                if let Some(key) = id {
+                    Ok(Persisted {
+                        id: Id(key.clone()),
+                        contents: self,
+                    })
+                } else {
+                    Err(E::new(format!("Unexpected Result: {:?}", res.result)))
+                }
+            }
+
+            fn get(c: &mut Connection, id: &Id) -> Result<Option<Persisted<Self>>, E> {
+                let res = try!(Rethink::table(Self::table()).get(id.str()).run(c));
+                if res.response_type == Response_ResponseType::RUNTIME_ERROR {
+                    return Err(E::new(format!("{:?}", res.result)));
+                }
+                println!("{:?}", res);
+
+                let r = res.result.get(0).map(|o| {
+                    Self::from_db(o).map(|c| {
+                        Persisted {
+                            id: id.clone(),
+                            contents: c,
+                        }
+                    })
+                });
+
+                match r {
+                    None => Ok(None),
+                    Some(Ok(p)) => Ok(Some(p)),
+                    Some(Err(e)) => Err(e),
+                }
+            }
+        }
+
+        #[derive(Debug)]
+        pub struct Persisted<T: Persistable> {
+            pub id: Id,
+            pub contents: T,
+        }
+    }
+
     pub mod auth {
         extern crate rand;
         use rand::Rng;
 
-        use rustc_serialize::json::{self, ToJson};
-
-        #[derive(RustcEncodable)]
         pub struct Token {
-            token: String
+            token: String,
         }
 
         impl Token {
             pub fn new() -> Token {
                 let mut rnd = rand::OsRng::new().unwrap();
                 let token = rnd.gen_ascii_chars().take(64).collect();
-                Token{token: token}
-            }
-
-            pub fn to_json(&self) -> json::Json {
-                json::Json::String(self.token.clone())
+                Token { token: token }
             }
         }
     }
 
     pub mod user {
-        extern crate rethinkdb;
-        use rethinkdb::RethinkDB;
-        use rethinkdb::r::*;
-
-        use std::rc::Rc;
-        use super::auth;
-
         use super::super::error::E;
 
-        #[derive(Debug, RustcDecodable, RustcEncodable)]
+        use rethink::Datum;
+        use super::persisted::Persistable;
+
+
+        #[derive(Debug, Serialize, Deserialize)]
         pub struct User {
             name: String,
         }
 
-        use std::collections::BTreeMap;
-        use rustc_serialize::json::{self, ToJson};
-        impl  User {
-            pub fn to_map(&self) -> BTreeMap<String, json::Json> {
-                let mut d = BTreeMap::new();
-                d.insert("name".to_string(), self.name.to_json());
-                d
+        impl User {
+            pub fn new<S: Into<String>>(name: S) -> Self {
+                User { name: name.into() }
+            }
+        }
+
+        impl Persistable for User {
+            fn table() -> &'static str {
+                "users"
             }
 
-            pub fn insert(&self, mut conn: &mut RethinkDB) -> Result<auth::Token, E> {
-                let mut map = self.to_map();
-                let token = auth::Token::new();
-                map.insert("auth_token".to_string(), token.to_json());
-                try!(db("test").table("users").
-                     insert(vec![json::Json::Object(map)]).run(&mut conn));
-                Ok(token)
+            fn to_db(&self) -> Datum {
+                Datum::Object(vec![("name".into(), Datum::String(self.name.clone()))]
+                                  .into_iter()
+                                  .collect())
             }
+
+            fn from_db(d: &Datum) -> Result<Self, E> {
+                let user = if let Datum::Object(ref o) = *d {
+                    o.get("name").and_then(|name| {
+                        if let Datum::String(ref s) = *name {
+                            Some(User { name: s.clone() })
+                        } else {
+                            None
+                        }
+                    })
+                } else {
+                    None
+                };
+
+                user.ok_or_else(|| E::new(format!("Couldn't make user from {:?}", d)))
+            }
+        }
+
+        #[cfg(test)]
+        mod tests {
+            extern crate rand;
+            use rand::Rng;
+
+            use rethink::Rethink;
+
+            use super::User;
+            use super::super::persisted::Persistable;
+
+            #[test]
+            fn test_insert() {
+                let mut rnd = rand::OsRng::new().unwrap();
+                let name: String = rnd.gen_ascii_chars().take(64).collect();
+
+                let mut rc = Rethink::connect_default().unwrap();
+                let user = User::new(name);
+                let persisted_user = user.insert(&mut rc).expect("insert the user");
+                println!("{:?}", persisted_user);
+
+                let fetched = User::get(&mut rc, &persisted_user.id)
+                                  .expect("get the user")
+                                  .unwrap();
+                assert_eq!(fetched.id, persisted_user.id);
+                assert_eq!(fetched.contents.name, persisted_user.contents.name);
+            }
+        }
+    }
+
+    pub mod event {
+        use super::user::User;
+
+        #[derive(Debug, Serialize, Deserialize)]
+        pub enum Guest {
+            Invited(User),
+            Attending(User),
+            Rejected(User),
+        }
+
+        #[derive(Debug, Serialize, Deserialize)]
+        pub struct Event {
+            name: String,
+            guests: Vec<Guest>,
+            id: String,
         }
     }
 }
 
-/*
-fn add_user(request: &mut nickel::Request) -> Result<models::auth::Token, error::E> {
-    let user = try!(request.json_as::<models::user::User>());
-    let mut conn = request.db_conn();
-    let token = try!(user.insert(&mut *conn));
-    Ok(token)
-}
-
-fn response<T: Encodable>(r: Result<T,error::E>) -> (StatusCode, String) {
-    match r.and_then(|t| json::encode(&t).map_err(|e| error::E::from(e))) {
-        Ok(t) => (StatusCode::Ok, t),
-        Err(e) => (StatusCode::BadRequest, e.description().to_string()),
-    }
-}
-*/
-
 fn main() {
-    //let dbmiddleware = db::RethinkdbMiddleware::new("localhost", 28015, "", 5);
-    //server.utilize(dbmiddleware);
-
+    use models::persisted::{Persistable, Id};
 
     let mut router = Router::new();
 
-
     router.route(Method::Get, "/user/:userid", |req: &mut Request| {
-        let ref query = req.extensions.get::<Router>().unwrap().find("userid").unwrap();
-        Ok(Response::with((Status::Ok, format!("This is user: {}", query))))
+        let mut rc = Rethink::connect_default().unwrap();
+        let query = req.extensions.get::<Router>().unwrap().find("userid").unwrap();
+        let user = itry!(models::user::User::get(&mut rc, &Id::from_string(query)));
+        Ok(Response::with((Status::Ok, format!("This is user: {:?}", user))))
     });
 
 
     router.route(Method::Post, "/users", |request: &mut Request| {
-        response(add_user(request))
+        #[derive(Debug, Deserialize)]
+        struct Body {
+            name: String,
+        }
+
+        let mut rc = Rethink::connect_default().unwrap();
+
+        let body: Body = itry!(serde_json::de::from_reader(&mut request.body));
+        let user = models::user::User::new(body.name);
+        let persisted_user = itry!(user.insert(&mut rc));
+        Ok(Response::with((Status::Ok, format!("Creating a user: {:?}", persisted_user))))
     });
 
+    println!("Running...");
     Iron::new(router).http("localhost:6767").unwrap();
 }
